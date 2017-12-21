fdc7d1251f127e109e9448f1f75ded9a23f98fbe
unknown <karaleina@gmail.com>
2017-12-21 11:37:33 +0100

    normalized

--- generator.py
+++ generator.py
@@ -1,6 +1,7 @@
 import numpy as np
 import cv2
 import math
+from matplotlib import pyplot as plt
 
 class PatternGenerator(object):
 
@@ -23,6 +24,9 @@ class PatternGenerator(object):
         for index, row in enumerate(image):
             image[index] = y_single_row
 
+        # Scaling sinus to 0-1
+        image = (image + 1) / 2
+
         return image
 
     def calculate_horizontal_pattern(self, phi, T, w, h):
@@ -30,30 +34,36 @@ class PatternGenerator(object):
 
     def pattern_sin(self, w, h, phases=[], periods=[], directions=[]):
         images = []
+        names = []
         for direction in directions:
             for period in periods:
                 for phi in phases:
-                    if direction == 0:
+                    if direction == 90:
                         images.append(self.calculate_vertical_pattern(phi, T=period, w=w, h=h))
-                    elif direction == 90:
+                        names.append("phi=" + str(phi) + " T=" + str(period) + " direct=" + str(direction))
+                    elif direction == 0:
                         images.append(self.calculate_horizontal_pattern(phi, T=period, w=w, h=h))
-        return images
+                        names.append("phi=" + str(phi) + " T=" + str(period) + " direct=" + str(direction))
+
+        return images, names
 
 
 
 if __name__ == "__main__":
     pg = PatternGenerator()
-    pattern = pg.calculate_horizontal_pattern(phi=20, T=20, w=500, h=400)
-    print(pattern)
-    cv2.imshow("Pattern_horizontal", pattern)
-    pattern2 = pg.calculate_vertical_pattern(phi=20, T=20, w=500, h=400)
-    print(pattern2)
-    cv2.imshow("Pattern_vertical", pattern2)
 
-    images = pg.pattern_sin(w=500, h=400, phases=[0, 10, 30], periods=[8, 90, 50], directions=[0, 90])
-    for index, image in enumerate(images):
-        cv2.imshow("Pattern" + str(index), pattern)
+    # pattern = pg.calculate_horizontal_pattern(phi=20, T=20, w=500, h=400)
+    # print(pattern)
+    # cv2.imshow("Pattern_horizontal", pattern)
+    # pattern2 = pg.calculate_vertical_pattern(phi=20, T=20, w=500, h=400)
+    # print(pattern2)
+    # cv2.imshow("Pattern_vertical", pattern2)
 
+    phases = list(np.linspace(0, 30, num=2)) # radiany
+
+    images, names = pg.pattern_sin(w=800, h=600, phases=phases, periods=[160], directions=[0])
+    for index, (image, name) in enumerate(zip(images, names)):
+        cv2.imshow("Pattern_" + str(name), image)
     cv2.waitKey()
 
     # TODO add information about pattern
